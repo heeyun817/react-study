e16bc84733ee2a65e7c68722ff404e111fde6d01
heeyun817 <vmffpfm79@naver.com>
2024-01-16 00:51:24 +0900

    컴포넌트만들기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-
+function Header(){ //사용자 정의 태그(컴포넌트)를 만들 때는 반드시 대문자로 시작해야함
+  return <header>
+  <h1><a href='/'>WEB</a></h1>
+</header>
+}
+function Nav(){ 
+  return <nav>
+  <ol>
+    <li><a href='/read/1'>html</a></li>
+    <li><a href='/read/2'>css</a></li>
+    <li><a href='/read/3'>js</a></li>
+  </ol>
+</nav>
+}
+function Article(){ 
+  return <article>
+  <h2>Welcome</h2>
+  Hello, WEB
+</article>
+}
 function App() {
   return (
-    <div className="App">
-      Hello React!
+    <div>
+      <Header></Header>
+      <Nav></Nav>
+      <Article></Article>
     </div>
   );
 }
