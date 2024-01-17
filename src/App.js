e2784c2780720cc695f6a81529240d09adfb499a
heeyun817 <vmffpfm79@naver.com>
2024-01-17 21:00:24 +0900

    Create

--- src/App.js
+++ src/App.js
@@ -34,14 +34,30 @@ function Article(props){
   {props.body}
 </article>
 }
+function Create(props){
+  return <article>
+  <h2>Create</h2>
+  <form onSubmit={event=>{
+    event.preventDefault();
+    const title = event.target.title.value; //target : event를 유발 시킨 태그 -> form태그
+    const body = event.target.body.value;
+    props.onCreate(title, body);
+  }}> {/*submin버튼을 클릭했을 때 form태그에서 발생하는 이벤트*/}
+    <p><input type="text" name="title" placeholder='title'/></p>
+    <p><textarea name='body' placeholder='body'></textarea></p>
+    <p><input type='submit' value="Create"></input></p>
+  </form>
+</article>
+}
 function App() {
   const [mode, setMode] = useState('WELCOME');
   const [id, setId] = useState(null);
-  const topics = [
+  const [nextId, setNextId] = useState(4);
+  const [topics, setTopics] = useState([
     {id:1, title:'html', body:'html is ...'},
     {id:2, title:'css', body:'css is ...'},
     {id:3, title:'javascript', body:'javascript is ...'}
-  ]
+  ])
   let content = null;
   if(mode === 'WELCOME'){
     content = <Article title="Welcome" body="Hello, WEB"></Article>
@@ -55,6 +71,16 @@ function App() {
       }
     }
     content = <Article title={title} body={body}></Article>
+  } else if(mode === 'CREATE'){
+    content = <Create onCreate={(_title, _body)=>{
+      const newTopic = {id:nextId,title:_title, body:_body}
+      const newTopics = [...topics]
+      newTopics.push(newTopic);
+      setTopics(newTopics);
+      setMode('READ');
+      setId(nextId);
+      setNextId(nextId+1);
+    }}></Create>
   }
 
   return (
@@ -67,6 +93,10 @@ function App() {
         setId(_id);
       }}></Nav>
       {content}
+      <a href='/create' onClick={event=>{
+        event.preventDefault();
+        setMode('CREATE')
+      }}>Create</a>
     </div>
   );
 }
